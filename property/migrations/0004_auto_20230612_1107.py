1a849c238009afde9f1694af6cfc2697d78f584c
Vladimir <vladimir.papin@gmail.com>
2023-06-16 10:01:47 +0300

    use update method in migration 004

--- property/migrations/0004_auto_20230612_1107.py
+++ property/migrations/0004_auto_20230612_1107.py
@@ -5,12 +5,8 @@ from django.db import migrations
 
 def set_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-        else:
-            flat.new_building = False
-        flat.save()
+    Flat.objects.all().update(new_building=False)
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
 
 
 class Migration(migrations.Migration):
